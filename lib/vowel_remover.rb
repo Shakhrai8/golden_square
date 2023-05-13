b80a10f6abcbeef0e2283242710a71c63ac3db13
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-13 17:01:47 +0100

    passes all tests

--- lib/vowel_remover.rb
+++ lib/vowel_remover.rb
@@ -5,13 +5,12 @@ class VowelRemover
   end
 
   def remove_vowels()
-    i = 0
-    while i < @text.length()
-      binding.irb
+    i = @text.length - 1
+    while i >= 0
       if @vowels.include? @text[i].downcase
         @text = @text.slice(0,i) + @text.slice(i+1..-1)
       end
-      i += 1
+      i -= 1
     end
     return @text
   end
