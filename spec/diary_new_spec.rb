05c5e3d6c508c6122c75cacc2c36e68c0f721a8f
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-15 11:48:01 +0100

    add test unit for Diary class, passes tests

--- spec/diary_new_spec.rb
+++ spec/diary_new_spec.rb
@@ -1,1 +1,10 @@
 require 'diary_new'
+
+RSpec.describe Diary do
+  context "initialy" do
+    it "should return an empty array" do
+      diary = Diary.new
+      expect(diary.all).to eq []
+    end
+  end
+end
