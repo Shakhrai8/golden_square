05f0f18bcb4a3abff5211e50270a364f6346bfb7
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-16 11:21:57 +0100

    reformat the code

--- spec/diary_entry_new_spec.rb
+++ spec/diary_entry_new_spec.rb
@@ -1,23 +1,24 @@
 require 'diary_entry_new'
 
 RSpec.describe DiaryEntry do
+  let(:entry_1) { DiaryEntry.new("Monday", "Lorem ipsum.") }
+  let(:entry_2) { DiaryEntry.new("Monday", "") }
+  let(:entry_3) { DiaryEntry.new("Title", "one two three") }
+  
   describe "when creating a new diary entry" do
     it "returns a diary entry" do
-      diary_entry = DiaryEntry.new("Monday", "Lorem ipsum.")
-      expect(diary_entry.title).to eq("Monday")
-      expect(diary_entry.contents).to eq("Lorem ipsum.")
+      expect(entry_1.title).to eq("Monday")
+      expect(entry_1.contents).to eq("Lorem ipsum.")
     end
   end
 
   describe "when given a diary entry" do
     it "returns the number of words in the entry" do
-      diary_entry = DiaryEntry.new("Monday", "Lorem ipsum.")
-      expect(diary_entry.count_words).to eq(2)
+      expect(entry_1.count_words).to eq(2)
     end
 
     it "returns 0 for an empty entry" do
-      diary_entry = DiaryEntry.new("Monday", "")
-      expect(diary_entry.count_words).to eq(0)
+      expect(entry_2.count_words).to eq(0)
     end
   end
 
@@ -36,15 +37,14 @@ RSpec.describe DiaryEntry do
   end
 
   describe "when given a reading speed and number of minutes" do
-    let(:diary_entry) { DiaryEntry.new("Title", "one two three") }
 
     it "returns a chunk for a given wpm and minutes" do
-      expect(diary_entry.reading_chunk(1, 2)).to eq("one two")
-      expect(diary_entry.reading_chunk(2, 1)).to eq("three")
+      expect(entry_3.reading_chunk(1, 2)).to eq("one two")
+      expect(entry_3.reading_chunk(2, 1)).to eq("three")
     end
 
     it "fails when given negative wpm" do
-      expect { diary_entry.reading_chunk(-1, 1) }.to raise_error(RuntimeError, "Wpm value must be above 0!")
+      expect { entry_3.reading_chunk(-1, 1) }.to raise_error(RuntimeError, "Wpm value must be above 0!")
     end
   end
 end
--- spec/diary_new_spec.rb
+++ spec/diary_new_spec.rb
@@ -1,14 +1,13 @@
 require 'diary_new'
 
 RSpec.describe Diary do
+  let(:diary) { Diary.new }
   context "initialy" do
     it "should return an empty array" do
-      diary = Diary.new
       expect(diary.all).to eq []
     end
 
     it "should return 0" do
-      diary = Diary.new
       expect(diary.count_words).to eq 0
     end
   end
